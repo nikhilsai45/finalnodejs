caeeff931930aa0816f7387155d565e94da8cb48
nikhilsai45 <nikhilsai.kamma@gmail.com>
2023-12-04 14:01:47 -0500

    commited

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const {MongoClient} = require('mongodb');
 const uri ="mongodb+srv://nikhil:Chowdary%233@cluster0.beose9h.mongodb.net/";
 const client = new MongoClient(uri);
 
-http.createServer(async(req,res) => {
-   
+const server   =http.createServer(async(req,res) => {
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+    
+      
+      };  
 const connectDB=async()=>{
     try{
         await client.connect();
@@ -21,11 +26,11 @@ connectDB();
 
 console.log(req.url)
 if(req.url === '/'){
-    fs.readFile( path.join(__dirname,'public','index.html'),(err,content)=>{
+    fs.readFile( path.join(__dirname,'public','index.html'),(err,data)=>{
 
     if (err) throw err;
     res.writeHead(200,{ 'Content-Type' : 'text/html'});
-    res.end(content);
+    res.end(data);
     }
  )
  
@@ -38,7 +43,7 @@ else if(req.url=='/api')
     const results = await cursor.toArray();
     //console.log(results);
     const js= (JSON.stringify(results));
-    res.writeHead(200,{ 'Content-Type' : 'application/json'});
+    res.writeHead(200,headers)
     console.log(js);
     res.end(js);
 
